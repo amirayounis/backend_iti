9e1b4be4c054c6d5a1a4fa98362fd0c95ecd7c50
Marwa Samy <marwasamy0998@gmail.com>
2025-11-19 10:28:39 +0200

    Interview Added

--- src/jobs/interview_views/end_interview.py
+++ src/jobs/interview_views/end_interview.py
@@ -1,6 +1,7 @@
+import datetime
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from jobs.models import Interview
+from jobs.models import Interview, JobPost
 from ..interview_services.report_generator import ReportGenerator
 from ..interview_services.conversation import ConversationService
 
@@ -10,16 +11,18 @@ class EndInterviewView(APIView):
         conversation_id = request.data["conversation_id"]
         interview = Interview.objects.get(conversation_id=conversation_id)
         items = ConversationService.get_conversation(conversation_id)
+        job = JobPost.objects.get(id=interview.job_id)
+        requirements = f"{job.title}: {job.description} , Required Skills: {', '.join([skill.name for skill in job.required_skills.all()])}"
         # build transcript text
         transcript = ""
         for msg in items:
             transcript += f"{msg.role.upper()}: {msg.content[0].text}\n"
 
         report_data = ReportGenerator.generate_report(
-            job_requirements=interview.job.requirements,
+            job_requirements=requirements,
             transcript=transcript
         )
-
+        interview.ended_at=datetime.now()
         interview.score = report_data["score"]
         interview.report = report_data["summary"]
         interview.status = "finished"
